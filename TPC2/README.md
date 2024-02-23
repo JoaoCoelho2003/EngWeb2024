92ba5edd489dc253874c579472312b9da1ef3dff
João Coelho <jhcoelho03@gmail.com>
2024-02-23 16:45:39 +0000

    feat: completed TPC2

---
+++ TPC2/README.md
@@ -0,0 +1,23 @@
+# TPC2: Página HTML das Cidades
+
+## Autor
+
+**Nome:** João Coelho
+
+**Número:** A100596
+
+## Enunciado
+
+- Desenvolvimento de uma página web de índice que lista todas as cidades por *ordem alfabética* com links para páginas individuais de cada cidade;
+- Criação de páginas individuais para cada cidade com detalhes sobre a população, descrição, distrito e conexões com outras cidades;
+- Nas conexões, têm de haver links para as cidades conectadas, bem como a distância entre a cidade de origem e destino.
+
+## Instruções de Uso
+
+Para criar as páginas *.html* é necessário correr o seguinte comando:
+    **python3 generateHtml.py**
+
+De seguida, precisa de iniciar o servidor Node.js:
+    **node resolucao.js**
+
+Por fim, basta ir ao *File Explorer* e clicar no ficheiro index.html.
--- TPC2/generateHtml.py
+++ TPC2/generateHtml.py
@@ -11,13 +11,17 @@ def open_json(file):
 
 # create the index page with the links to the other pages, the link is the name of the City in the json file
 
-def create_index(data,index_page):
+def create_index(data, index_page):
     index = open("index.html", "w")
     index_page += '<h1 class=\"bg-slate-200 py-4 text-3xl font-semibold text-white\" style=\"background-color: #6a0303;text-align: center;\">Cidades</h1>'
     index_page += "<ul>"
-    for city in data['cidades']:
-        # access the colletions "Cidades" and then "nome"
-        index_page += f"<li><a href=\"http://localhost:1902/{city['id']}.html\">{city['nome']}</a></li>"
+    
+    # Sort cities alphabetically by name
+    sorted_cities = sorted(data['cidades'], key=lambda x: x['nome'])
+    
+    for city in sorted_cities:
+        index_page += f"<li><a href=\"http://localhost:1902/{city['id']}.html\"><u style='color: #6a0303;'>{city['nome']}</u></a></li><br>"
+
     index_page += "</ul>"
     index_page += "</body></html>"
     index.write(index_page)
@@ -41,9 +45,9 @@ def create_CityPages(data):
         """
         city_page += f"<h1 class=\"bg-slate-200 py-4 text-3xl font-semibold text-white\" style=\"background-color: #6a0303;text-align: center;\">{city['nome']}</h1>"
         # população, descrição, e distrito
-        city_page += f"<p>População: {city['população']}</p>"
-        city_page += f"<p>Descrição: {city['descrição']}</p>"
-        city_page += f"<p>Distrito: {city['distrito']}</p>"
+        city_page += f"<span style=\"color: #6a0303; font-weight: bold; margin-bottom: 10px; display: block;\">População:</span> {city['população']} habitantes<br>"
+        city_page += f"<span style=\"color: #6a0303; font-weight: bold; margin-bottom: 10px; display: block;\">Descrição:</span> {city['descrição']}<br>"
+        city_page += f"<span style=\"color: #6a0303; font-weight: bold; margin-bottom: 10px; display: block;\">Distrito:</span> {city['distrito']}<br>"
         city_page += "<h2 class=\"bh-slate-100 py-12 text-3xl font-semibold\" style=\"color: #6a0303;text-align: center;\">Ligações</h2>"
         city_page += "<ul>"
         cityName = ""
@@ -53,10 +57,16 @@ def create_CityPages(data):
                 for cities in data['cidades']:
                     if cities['id'] == ligacao['destino']:
                         cityName = cities['nome']
-                city_page += f"<li><a href=\"http://localhost:1902/{ligacao['destino']}.html\">{cityName}</a></li>"
-                city_page += f"<p>Distância: {ligacao['distância']}</p>"
+                city_page += f"<span style=\"color: #6a0303; font-weight: bold; margin-bottom: 10px;\">Ligação com: </span>"
+                city_page += f"<span style=\"margin-bottom: 10px;\"><a href=\"http://localhost:1902/{ligacao['destino']}.html\"><span style='text-decoration-color: #6a0303; text-decoration-style: underline;'>{cityName}</span></a></span><br><br>"
+                city_page += f"<span style=\"color: #6a0303; font-weight: bold; margin-bottom: 10px;\">Distância:</span> {ligacao['distância']} km<br><br>"
+                city_page += f"<span class=\"separator\"></span><br>"
 
         city_page += "</ul>"
+        city_page += "<div style='text-align: center; margin-top: 20px; margin-bottom: 20px;'>"
+        city_page += "<h6><a href='../' style='display: inline-block; padding: 10px 20px; background-color: #6a0303; color: white; text-decoration: none; border-radius: 5px;'>Voltar</a></h6>"
+        city_page += "</div>"
+        city_page += "<div style='background-color: #6a0303; height: 100px;'></div>"
         city_page += "</body></html>"
         with open(f'Pages/{city["id"]}.html', 'w', encoding='utf-8') as file:
             file.write(city_page)
